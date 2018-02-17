634ccaf077d8ba5f311a4314425631b042ded898
rob-now <rnowakowski.prv@gmail.com>
2018-02-17 10:21:49 +0100

    Added piToNDecimalPlaces.js

---
+++ piToNDecimalPlaces.js
@@ -0,0 +1,19 @@
+/*
+Given a number n, write a function that returns PI to n decimal places.
+
+Examples
+5 ➞ 3.14159
+4 ➞ 3.1416
+15 ➞ 3.141592653589793
+
+Notes
+- n will not be above 15, to keep this challenge simple.
+- Round up the last digit if the next digit in PI is greater or equal to 5 (see the second example above).
+- The return value must be a decimel - not a string.
+ */
+
+function myPi(n) {
+    return Number(Math.PI.toFixed(n));
+}
+
+console.log(myPi(4));
