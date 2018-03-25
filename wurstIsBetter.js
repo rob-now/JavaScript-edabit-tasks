1d54ad4b6637a5022f050fd3ff1d7f90935e88d6
rob-now <rnowakowski.prv@gmail.com>
2018-03-25 15:00:53 +0200

    Added wurstIsBetter.js

---
+++ wurstIsBetter.js
@@ -0,0 +1,42 @@
+/*
+Wurst is the best. Create a function that takes a string and replaces every mention of any type of sausage with the German word "Wurst," unless—of course—the sausage is already a type of German "Wurst" (i.e. "Bratwurst", see below), then leave the sausage name unchanged.
+
+Examples of German Wursts
+Bratwurst
+Kochwurst
+Leberwurst
+Mettwurst
+Rostbratwurst
+
+List of sausages that need to turn into "Wurst"
+Kielbasa
+Chorizo
+Moronga
+Salami
+Sausage
+Andouille
+Naem
+Merguez
+Gurka
+Snorkers
+Pepperoni
+
+Rules
+Replace every mention of any sausage from the above list, with the word "Wurst"
+Do not replace any German sausage with the word "Wurst"
+The word "Wurst" must be titlecase
+Return the Wurst-ified string
+
+Examples
+"I like chorizos, but not sausages" ➞ "I like Wursts, but not Wursts"
+"Bratwurst and Rostbratwurst are sausages" ➞ "Bratwurst and Rostbratwurst are Wursts"
+
+Notes
+All test cases contain valid strings. All German sausage names in test cases contain the word "wurst".
+ */
+
+function wurstIsBetter(str) {
+    return str.replace(/Kielbasa|Chorizo|Moronga|Salami|Sausage|Andouille|Naem|Merguez|Gurka|Snorkers|Pepperoni/gi, "Wurst");
+}
+
+console.log(wurstIsBetter("I like chorizos, but not sausages"));
