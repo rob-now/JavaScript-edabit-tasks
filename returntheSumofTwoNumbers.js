edf02875ff33b6faf8029163d3699ef5ccd9b268
rob-now <rnowakowski.prv@gmail.com>
2018-03-25 10:18:08 +0200

    Added returntheSumofTwoNumbers.js

---
+++ returntheSumofTwoNumbers.js
@@ -0,0 +1,8 @@
+function addition(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "Sorry but you didn't pass two numbers.";
+    }
+    return a + b;
+}
+
+console.log(addition(4));
