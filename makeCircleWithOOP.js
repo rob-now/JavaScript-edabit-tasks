fae2dce5a20b1320b676893447364603cf01972f
rob-now <rnowakowski.prv@gmail.com>
2018-03-25 11:16:01 +0200

    Added makeCircleWithOOP.js

---
+++ makeCircleWithOOP.js
@@ -0,0 +1,21 @@
+/*
+Your task is to create a Circle constructor that creates a circle with a radius provided by an argument. The circles constructed must have two getters getArea() (PIr^2) and getPerimeter() (2PI*r) which give both respective areas and perimeter (circumference).
+ */
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    getArea() {
+        return Math.PI * Math.pow(this.radius, 2)
+    }
+
+    getPerimeter() {
+        return 2 * Math.PI * this.radius
+    }
+}
+
+let q = new Circle(11);
+console.log(q.getArea());
+console.log(q.getPerimeter());
